efa0998db16e23f4f12e78ad2517a960e234cc68
asifahmed1810 <asifahmed18oct@gmail.com>
2025-10-05 12:25:05 +0600

    Added initial setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+   
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Button>Click</Button>
+    </>
+  )
+}
+
+export default App
